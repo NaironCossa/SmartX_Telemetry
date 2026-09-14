517d17d19cc1d0d181526b19c3ec0a5e85af6790
emeris <emeris@LabVM2302769>
2026-09-14 03:23:35 +0200

    Add DeviceNode model to represent nested IoT network topologies

---
+++ SmartX_Telemetry/Models/DeviceNode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace SmartX_Telemetry.Models
+{
+    public class DeviceNode
+    {
+        public string NodeName { get; set; }
+        public bool IsSafelyConfigured { get; set; }
+        public List<DeviceNode> Children { get; set; }
+
+        public DeviceNode(string nodeName, bool isSafelyConfigured = true)
+        {
+            NodeName = nodeName;
+            IsSafelyConfigured = isSafelyConfigured;
+            Children = new List<DeviceNode>(); // Initialising the generic collection for nested children
+        }
+
+        public void AddChild(DeviceNode child)
+        {
+            Children.Add(child);
+        }
+    }
+}
