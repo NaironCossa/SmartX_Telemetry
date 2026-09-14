5fb9315d937c1e65c59147d8b805e331e7c9f7c6
emeris <emeris@LabVM2302769>
2026-09-14 16:57:32 +0200

    Wire up sidebar navigation to load IngestionView dynamically

--- SmartX_Telemetry/MainWindow.xaml.cs
+++ SmartX_Telemetry/MainWindow.xaml.cs
@@ -1,24 +1,25 @@
-﻿using System.Text;
+﻿using SmartX_Telemetry.Views;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SmartX_Telemetry
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            // Wire up the button click event
+            BtnIngestion.Click += BtnIngestion_Click;
+
+            // Load the ingestion view by default on startup
+            MainFrame.Navigate(new IngestionView());
+        }
+
+        private void BtnIngestion_Click(object sender, RoutedEventArgs e)
+        {
+            // Navigate the central frame to our custom UserControl
+            MainFrame.Navigate(new IngestionView());
         }
     }
 }
