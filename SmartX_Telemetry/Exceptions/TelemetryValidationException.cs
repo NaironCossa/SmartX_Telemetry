7fb25e8cef81022c1b73f35e678353e0f37b9d64
emeris <emeris@LabVM2302769>
2026-09-14 17:07:53 +0200

    Add custom TelemetryValidationException for secure error handling

---
+++ SmartX_Telemetry/Exceptions/TelemetryValidationException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartX_Telemetry.Exceptions
+{
+    // Inheriting from the base Exception class
+    public class TelemetryValidationException : Exception
+    {
+        public TelemetryValidationException()
+            : base("Critical failure: The telemetry data failed validation.") { }
+
+        public TelemetryValidationException(string message)
+            : base(message) { }
+
+        public TelemetryValidationException(string message, Exception innerException)
+            : base(message, innerException) { }
+    }
+}
